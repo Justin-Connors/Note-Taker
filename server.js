127df079aac0e4aedd5f0d6377515fe9a93e1c41
justin connors <justinconnors@hotmail.ca>
2023-04-01 18:30:14 -0300

    stuck, not sure why like says cannot GET /notes ..... when it exists and paths are correct

--- server.js
+++ server.js
@@ -13,13 +13,9 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-});
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
-});
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
 
 const readFromFile = util.promisify(fs.readFile);
 const writeToFile = (destination, content) =>
@@ -63,6 +59,4 @@ app.post('/api/notes', (req, res) => {
     }
 });
 
-app.listen(PORT, () => 
-    console.log(`Nasa now has control of your PC and is hacking on ${PORT} 👀`)
-);
+app.listen(PORT, () => console.log(`Nasa now has control of your PC and is hacking on ${PORT} 👀`));
