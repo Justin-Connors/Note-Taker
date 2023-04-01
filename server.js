36d42d6ccbde2887300924ec39a4cb3933033601
justin connors <justinconnors@hotmail.ca>
2023-04-01 20:23:04 -0300

    added app.delete to work

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
-const util = require('util');
-const uuid = require('./helpers/uuid');
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const util = require("util");
+const uuid = require("./helpers/uuid");
 
 const PORT = process.env.PORT || 3001;
 
@@ -11,11 +11,15 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
 
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
 
 const readFromFile = util.promisify(fs.readFile);
 const writeToFile = (destination, content) =>
@@ -24,39 +28,59 @@ const writeToFile = (destination, content) =>
   );
 
 const readAndAppend = (content, file) => {
-    fs.readFile(file, 'utf8', (err, data) => {
-      if (err) {
-        console.error(err);
-      } else {
-        const parsedData = JSON.parse(data);
-        parsedData.push(content);
-        writeToFile(file, parsedData);
-      }
-    });
-  };
+  fs.readFile(file, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      parsedData.push(content);
+      writeToFile(file, parsedData);
+    }
+  });
+};
 
-app.get('/api/notes', (req, res) => {
-    console.info(`${req.method} request recieved for notes`);
-    readFromFile('./db/db.json').then((note) => res.json(JSON.parse(note)));
+app.get("/api/notes", (req, res) => {
+  console.info(`${req.method} request recieved for notes`);
+  readFromFile("./db/db.json").then((note) => res.json(JSON.parse(note)));
 });
 
-app.post('/api/notes', (req, res) => {
-    console.info(`${req.method} request recieved to add a note`);
+app.post("/api/notes", (req, res) => {
+  console.info(`${req.method} request recieved to add a note`);
 
-    const { title, text } = req.body;
+  const { title, text } = req.body;
 
-    if (req.body) {
-        const newNote = {
-          title,
-          text,
-          id: uuid(),
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      id: uuid(),
     };
 
-    readAndAppend(newNote, './db/db.json');
+    readAndAppend(newNote, "./db/db.json");
     res.json(`Note added!`);
-    } else {
-        res.error(`oh no there was an error`)
-    }
+  } else {
+    res.error(`oh no there was an error`);
+  }
 });
 
-app.listen(PORT, () => console.log(`Nasa now has control of your PC and is hacking on ${PORT} 👀`));
+app.delete("/api/notes/:id", (req, res) => {
+  const id = req.params.id;
+  console.info(`${req.method} request recieved to delete note with id ${id}`);
+
+  readFromFile("./db/db.json")
+    .then((data) => {
+      const notes = JSON.parse(data);
+      const updatedNotes = notes.filter((note) => note.id !== id);
+
+      writeToFile("./db/db.json", updatedNotes);
+      res.json(`Note with id ${id} has been deleted`);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Internal server error");
+    });
+});
+
+app.listen(PORT, () =>
+  console.log(`Nasa now has control of your PC and is hacking on ${PORT} 👀`)
+);
